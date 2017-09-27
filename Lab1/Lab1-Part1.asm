f236f25d7a96e9a29ae371a779a09bd82dbbc234
anthonyrojas <anthony.rojas095@gmail.com>
2017-09-26 21:05:30 -0700

    Update Lab1-Part1.asm

--- Lab1/Lab1-Part1.asm
+++ Lab1/Lab1-Part1.asm
@@ -8,20 +8,28 @@
 numPrompt: .asciiz "How many numbers? (N)"
 multiplier: .asciiz "\nMultiplier? (y)"
 sumText: .asciiz "\nSum = "
+testText: .asciiz "Testing subroutine\n"	
 	.text
+	
 main:	li $v0, 4 #allow output string
 	la $a0, numPrompt
-	syscall
+	syscall #output string
 	li $v0, 5 #allow input integer
-	syscall
+	syscall #input integer
 	move $t0, $v0 #move input value of N into $t0
 	
 	li $v0, 4 #allow output string
 	la $a0, multiplier
-	li $v0, 5 #allow input integer
 	syscall
+	li $v0, 5 #allow input integer
+	syscall #input integer
 	move $t1, $v0 #move input value of y into $t1
 	
+	add $a0,$zero,$t0
+	add $a1, $zero, $t1 
+	jal summation #jump to summation
+	
+	li      $v0, 10              # terminate program run and
 	syscall
-	
-	
+summation:
+	jr $ra
