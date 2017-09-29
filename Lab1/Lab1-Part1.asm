aa56a5e54b3ea407f12151402cbf5140df2898f8
anthonyrojas <anthony.rojas095@gmail.com>
2017-09-28 21:11:19 -0700

    Summation needs work

--- Lab1/Lab1-Part1.asm
+++ Lab1/Lab1-Part1.asm
@@ -25,11 +25,21 @@ main:	li $v0, 4 #allow output string
 	syscall #input integer
 	move $t1, $v0 #move input value of y into $t1
 	
-	add $a0,$zero,$t0
-	add $a1, $zero, $t1 
+	add $a0,$zero,$t0 #a0=N
+	add $a1, $zero, $t1 #a1=y
+	addi $a2, $zero, 0 #a2=i initalize with 1
+	addi $a3, $zero, 0 #sum
 	jal summation #jump to summation
+	
+	li $v0, 1
+	move $a0, $a3
+	syscall
 	
 	li      $v0, 10              # terminate program run and
 	syscall
-summation:
+summation: 
+	addi $a2, $a2, 1 #increment i
+	mul $t0, $a1, $a0 #t0=y*i
+	add $a3, $a3, $t0 #sum of prod and current summation
+	bne $a2, $a0, summation #if(i<N) then loop summation algorithm
 	jr $ra
