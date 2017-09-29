8057132f62584237454aa3fc6253dd7660677c48
anthonyrojas <anthony.rojas095@gmail.com>
2017-09-28 23:17:31 -0700

    Done

--- Lab1/Lab1-Part1.asm
+++ Lab1/Lab1-Part1.asm
@@ -1,9 +1,3 @@
-#li $t2, 10
-#add $t1, $t2, 2
-#li $v0, 1
-#move $a0, $t1
-#syscall
-
 .data
 numPrompt: .asciiz "How many numbers? (N)"
 multiplier: .asciiz "\nMultiplier? (y)"
@@ -11,7 +5,7 @@ sumText: .asciiz "\nSum = "
 testText: .asciiz "Testing subroutine\n"	
 	.text
 	
-main:	li $v0, 4 #allow output string
+MAIN:	li $v0, 4 #allow output string
 	la $a0, numPrompt
 	syscall #output string
 	li $v0, 5 #allow input integer
@@ -27,19 +21,23 @@ main:	li $v0, 4 #allow output string
 	
 	add $a0,$zero,$t0 #a0=N
 	add $a1, $zero, $t1 #a1=y
-	addi $a2, $zero, 0 #a2=i initalize with 1
+	li $a2, 0
+	#addi $a2, $zero, 0 #a2=i initalize with 1
 	addi $a3, $zero, 0 #sum
-	jal summation #jump to summation
+	j SUMMATION #jump to summation
 	
+SUMMATION:
+	addi $a2, $a2, 1 #increment i
+	mul $t0, $a1, $a2 #t0=y*i
+	add $a3, $a3, $t0 #sum of prod and current summation
+	
+	bne $a2, $a0, SUMMATION #if(i!=N) then loop summation algorithm
+	j END
+	
+END:
 	li $v0, 1
 	move $a0, $a3
-	syscall
+	syscall #output final sum
 	
 	li      $v0, 10              # terminate program run and
 	syscall
-summation: 
-	addi $a2, $a2, 1 #increment i
-	mul $t0, $a1, $a0 #t0=y*i
-	add $a3, $a3, $t0 #sum of prod and current summation
-	bne $a2, $a0, summation #if(i<N) then loop summation algorithm
-	jr $ra
